126ccb5d9c088009febd8250d2b92202f95d6f7b
陈宇翔 <chen_yux@126.com>
2021-01-26 15:46:25 +0800

    ++es

---
+++ JUC/src/main/java/com/chen/es/Es.java
@@ -0,0 +1,17 @@
+package com.chen.es;
+
+import org.apache.lucene.util.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+
+/**
+ * Description :   .
+ *
+ * @author : chenyuxiang
+ * @date : Created in 2021/1/20 2:54 下午
+ */
+public class Es {
+
+	public static void main(String[] args) {
+
+	}
+}
